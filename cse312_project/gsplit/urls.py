fc9243d061fee734cb38c10ce7a090001793db26
Chris <cjperez2017@gmail.com>
2020-04-04 16:51:00 -0500

    switch to django

--- cse312_project/gsplit/urls.py
+++ cse312_project/gsplit/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.home_page, name='gsplit-home-page'),
-    path('profile/', views.profile_page, name='gsplit-profile-page'),
-    path('friends/', views.friends_page, name='gsplit-friends-page'),
-    path('post/', views.post_page, name='gsplit-post-page')
+    path('', views.index, name='gsplit-index'),
+    path('profile', views.profile, name='gsplit-profile'),
+    path('friends', views.friends, name='gsplit-friends'),
+    path('create_acc', views.create_acc, name='gsplit-create_acc'),
+    path('chat', views.chat, name='gsplit-chat'),
+    path('login', views.login, name='gsplit-login'),
+    # path('script', views.script, name='gsplit-script'),
+    # path('style', views.style, name='gsplit-style'),
+
 ]
--- cse312_project/gsplit/views.py
+++ cse312_project/gsplit/views.py
@@ -3,17 +3,35 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def home_page(request):
+def index(request):
     return render(request, 'gsplit/index.html')
 
 
-def profile_page(request):
+def profile(request):
     return render(request, 'gsplit/profile.html')
 
 
-def friends_page(request):
+def friends(request):
     return render(request, 'gsplit/friends.html')
 
 
-def post_page(request):
-    return render(request, 'gsplit/post.html')
+def chat(request):
+    return render(request, 'gsplit/chat.html')
+
+
+def create_acc(request):
+    return render(request, 'gsplit/create_acc.html')
+
+
+def login(request):
+    return render(request, 'gsplit/login.html')
+
+
+# def style(request):
+#     return render(request, 'gsplit/static/gsplit/style.css')
+#
+#
+# def script(request):
+#     return render(request, 'gsplit/static/gsplit/script.js')
+
+
