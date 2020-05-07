2331649faa99888dfe890277666795fff552e765
Mayank Jha <mayank@Mayanks-MacBook-Pro-2.local>
2020-05-07 15:17:08 -0400

    docker config complete

--- cse312_project/cse312_project/settings.py
+++ cse312_project/cse312_project/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'NAME': 'cse312',
         'USER': 'postgres',
         'PASSWORD': 'cse312',
-        'HOST': "127.0.0.1",
+        'HOST': "db",
         'PORT': '5432'
 
     },
