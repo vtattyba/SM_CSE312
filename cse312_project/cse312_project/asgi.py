b5c4bb4df78ae65a24e024743792db2b4ce5bf1b
Mayank Jha <mayank@Mayanks-MacBook-Pro-2.local>
2020-05-11 01:14:48 -0400

    configured docker to run with redis and channels layer

--- cse312_project/cse312_project/asgi.py
+++ cse312_project/cse312_project/asgi.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 
 import os
 
+from channels.layers import get_channel_layer
+
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cse312_project.settings')
